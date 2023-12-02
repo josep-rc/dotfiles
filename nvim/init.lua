55a7bc4d5784b42c48d2d877ccc91ebf285b8df1
Josep Peñalba <jopema7@gmail.com>
2023-12-02 19:41:08 +0100

    New config for neovim

---
+++ nvim/init.lua
@@ -0,0 +1,17 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.g.mapleader = ' '
+
+-- vim.keymap.set('n', '<D-s>', '<cmd>write<cr>', {desc = 'Guardar'})
+
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', {desc = 'Guardar'})
+
+
+
